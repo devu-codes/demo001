21a72c52a6f2fd3de914a6afde9b40e11998ae26
devu-codes <devanshpandey977@gmail.com>
2023-12-30 23:26:34 +0530

    First Commit

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products);
+  res.render('shop', {prods: adminData.products});
+});
+
+module.exports = router;
